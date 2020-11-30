7653a6a74eeb4aeed78b15a3874157b9d243ad75
alvino <aangelo2@illinois.edu>
2020-11-30 21:06:59 +0700

    made class that contains player's information

--- include/core/player.h
+++ include/core/player.h
@@ -1,10 +1,47 @@
 #ifndef FINAL_PROJECT_PLAYER_H
 #define FINAL_PROJECT_PLAYER_H
 
+namespace snakeladder {
 
-class player {
+/**
+ * This class contains the information of a player
+ */
+class Player {
+public:
+    /**
+     * Default class constructor
+     */
+    Player();
+
+    /**
+     * Constructor that sets a player's number
+     * @param num player's number
+     */
+    Player(int num);
+
+    /**
+     * Getter of player's number
+     * @return player_num
+     */
+    int GetPlayerNum();
+
+    /**
+     * Getter of player's current tile
+     * @return tile
+     */
+    int GetTile();
+
+    /**
+     * Sets the player's tile
+     * @param new_tile new tile
+     */
+    void SetTile(int new_tile);
+
+private:
+    int player_num;
+    int tile;
 
 };
-
+}   // namespace snakeladder
 
 #endif //FINAL_PROJECT_PLAYER_H
--- src/core/player.cpp
+++ src/core/player.cpp
@@ -1,1 +1,27 @@
 #include "../../include/core/player.h"
+
+namespace snakeladder {
+
+Player::Player() {
+    player_num = 0;
+    tile = 1;
+}
+
+Player::Player(int num) {
+    player_num = num;
+    tile = 1;
+}
+
+int Player::GetPlayerNum() {
+    return player_num;
+}
+
+int Player::GetTile() {
+    return tile;
+}
+
+void Player::SetTile(int new_tile) {
+    tile = new_tile;
+}
+
+}   // namespace snakeladder
